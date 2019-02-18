d5f3fbd06ac4f91e98755fe3c2fb4878625c175a
zivce <zivkovicmilos5@hotmail.com>
2019-02-18 09:02:08 +0100

    - Updated README.md

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ import './index.css'
 import logger from 'redux-logger';
 
 import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {createStore , combineReducers, applyMiddleware,compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import reducers from './reducers/index';
 import createSagaMiddleware from 'redux-saga';
 
 import saga from './saga';
 
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const sagaMiddleware = createSagaMiddleware();
 
 
-const store = createStore (
+const store = createStore(
     reducers,
     applyMiddleware(sagaMiddleware)
 )
@@ -27,10 +26,10 @@ sagaMiddleware.run(saga);
 
 ReactDOM.render(
 
-<Provider store = {store}>
-    <BrowserRouter >
-        <App />
-    </BrowserRouter>
-</Provider>
+    <Provider store={store}>
+        <BrowserRouter >
+            <App />
+        </BrowserRouter>
+    </Provider>
 
-, document.getElementById('root'))
+    , document.getElementById('root'))
